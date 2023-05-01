9903b0afb80fd550a07656f3d45b0a6a8d9b6a0a
Emin B <zSuckyLP@gmail.com>
2023-05-02 01:11:27 +0200

    added minor todo

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 // TODO Sessions - each scene has a minor 0 which is the css. if you leave the major scene, the session ends, otherwise when not in-game we show when the session started
 // ^ option name "Show overall game session when not in-game" 
 // TODO HRC & BTT Records in discord
-// TODO Ranked match score, button "View opponent ranked profile", show details in stage striking already (in discord rich presence, signalize that you are in stage striking as well)
+// TODO Ranked match score, button "Viw opponent ranked profile", show details in stage striking already (in discord rich presence, signalize that you are in stage striking as well)
+// TODO clean up melee.rs, move structs/enums away in coherent bundles
 
 // #![windows_subsystem = "windows"]
 #![feature(generic_const_exprs)]
